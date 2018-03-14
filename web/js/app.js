c2ca22b0feb3551e1864b8cec5abbf37cd8e0afa
Charleno Pires <charleno@gmail.com>
2018-03-14 12:23:57 -0300

    Firebase all Ready on Web

--- web/js/app.js
+++ web/js/app.js
@@ -2,13 +2,26 @@ function random(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+function changeColorOn(r, g, b) {
+  Array.from(document.getElementsByClassName("elem")).forEach(function (e, i) {
+    e.style.backgroundColor = "rgba("+ r +", "+ g +", "+ b +","+ 0.1*(i+1)*2 + ")";
+  });
+}
+
 function changeColor() {
-  var r = random(0, 255);
-  var g = random(0, 255);
-  var b = random(0, 255);
-  document.getElementsByClassName("elem")[0].style.backgroundColor = `rgba(${r}, ${g}, ${b}, 0.2)`;
-  document.getElementsByClassName("elem")[1].style.backgroundColor = `rgba(${r}, ${g}, ${b}, 0.4)`;
-  document.getElementsByClassName("elem")[2].style.backgroundColor = `rgba(${r}, ${g}, ${b}, 0.6)`;
-  document.getElementsByClassName("elem")[3].style.backgroundColor = `rgba(${r}, ${g}, ${b}, 0.8)`;
-  document.getElementsByClassName("elem")[4].style.backgroundColor = `rgba(${r}, ${g}, ${b}, 1.0)`;
+  var database = firebase.database();
+  database.ref('colors').remove();
+  var colors = {
+    r: random(0, 255),
+    g: random(0, 255),
+    b: random(0, 255)
+  };
+
+  changeColorOn(colors.r, colors.g, colors.b);
+  database.ref('colors').set({
+    r: colors.r,
+    g: colors.g,
+    b: colors.b
+  });
+
 }
