b1f35e6f8d2b870b979d7b6c552a01aa10e9a7f7
Charleno Pires <charleno@gmail.com>
2018-03-28 14:44:38 -0300

    Refactoring Widget List

--- app/colorapp/lib/main.dart
+++ app/colorapp/lib/main.dart
@@ -23,6 +23,17 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  
+  _MyHomePageState({this.columns});
+  final List<Widget> columns;
+
+  List<Widget> colorsPainel(int r, int g,int b){
+    for(var i=0; i<=4; i++){
+      columns.add(ColorColumn(r, g, b, 0.1*(i+1)*2));
+    }
+    return columns;
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -31,83 +42,47 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: 
       new Stack(
-        children: <Widget>[
-          new Center(
-            child: new Row(
-              children: <Widget> [
-                new Expanded(
-                  child: new Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      new Expanded(
-                        child: new Container(
-                          color: new Color.fromRGBO(64, 23, 211, 0.2),
-                        ),
+        children: 
+          colorsPainel(64, 23, 211),
+          // .add(
+          //   new Center(
+          //     child: new RaisedButton(
+          //       color: Colors.blue[900],
+          //       textColor: Colors.white,
+          //       child: new Text("Change Color"),
+          //       onPressed: (){ print("Pressionado!!!");},
+          //     ),
+          //   )
+        ),
+    );
+  }
+
+
+}
+
+//fromRGBO(64, 23, 211, 0.8),
+
+
+
+class ColorColumn extends StatelessWidget{
+  const ColorColumn(this.r, this.g, this.b, this.alpha);
+
+  final int r, g, b;
+  final double alpha;
+
+  @override
+  Widget build(BuildContext context){
+    return new Expanded(
+                child: new Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    new Expanded(
+                      child: new Container(
+                        color: new Color.fromRGBO(r, g, b, alpha),
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
-                new Expanded(
-                  child: new Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      new Expanded(
-                        child: new Container(
-                          color: new Color.fromRGBO(64, 23, 211, 0.4),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                new Expanded(
-                  child: new Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      new Expanded(
-                        child: new Container(
-                          color: new Color.fromRGBO(64, 23, 211, 0.6),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                new Expanded(
-                  child: new Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      new Expanded(
-                        child: new Container(
-                          color: new Color.fromRGBO(64, 23, 211, 0.8),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                new Expanded(
-                  child: new Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      new Expanded(
-                        child: new Container(
-                          color: new Color.fromRGBO(64, 23, 211, 1.0),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ]
-            ),
-          ),
-          new Center(
-            child: new RaisedButton(
-              color: Colors.blue,
-              textColor: Colors.white,
-              child: new Text("Change Color"),
-              onPressed: (){ print("Pressed!!!");},
-            ),
-          )
-        ],
-      ),
     );
   }
 }
